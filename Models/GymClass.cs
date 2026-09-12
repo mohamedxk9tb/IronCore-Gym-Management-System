08d17299656a915962f38413b2ba279635fbd010
MarwanAbulazm <abulazmm@gmail.com>
2026-09-12 15:03:06 +0300

    feat: implement  plans, subscriptions, payments, classes and Booking.

---
+++ Models/GymClass.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GymMvc.Models
+{
+    public class GymClass
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int TrainerId { get; set; }
+
+        [ForeignKey("TrainerId")]
+        public Trainer Trainer { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string DayOfWeek { get; set; }
+
+        [Required]
+        [Range(1, 100)]
+        public int Capacity { get; set; }
+
+        public List<Booking> Bookings { get; set; } = new List<Booking>();
+    }
+}
